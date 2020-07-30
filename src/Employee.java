8ce6f23f7b685000836fc73fa009b27433c46533
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-30 16:18:43 +0100

    Getter and Setter

--- src/Employee.java
+++ src/Employee.java
@@ -1,9 +1,23 @@
 public class Employee {
 
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+    public void setBaseSalary(int baseSalary) {
+        if(baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+            this.baseSalary = baseSalary;
+    }
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+    public int getHourlyRate() {
+        return hourlyRate;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(23000);
+        employee.getBaseSalary();
+        employee.setHourlyRate(20);
+        employee.getHourlyRate();
         int wage = employee.calculateWage(200);
 
         System.out.println(wage);
+        System.out.println(employee.getBaseSalary());
+        System.out.println(employee.getHourlyRate());
     }
 
 }
