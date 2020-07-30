ab0a2b5de9187f4e22b520e0b48299a63dc0c142
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-30 15:42:50 +0100

    Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int baseSalary = 50_000;
-        int extraHouse = 10;
-        int hourlyRate = 20;
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(200);
 
-        int wage = calculateWage(baseSalary,extraHouse,hourlyRate);
         System.out.println(wage);
     }
 
-    public static int calculateWage(int baseSalary, int extraHours, int hourlyRate){
-        return baseSalary + (extraHours * hourlyRate);
-    }
 }
